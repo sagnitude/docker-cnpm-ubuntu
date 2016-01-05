afa630e7b9231f44537a80bf7ea722b451e99955
sagnitude <leyicong@gmail.com>
2016-01-06 02:30:22 +0800

    b

--- install_cnpm.sh
+++ install_cnpm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-npm install -g --registry=http://registry.npm.taobao.org --disturl=http://npm.taobao.org/mirrors/node  cnpmjs.org cnpm sqlite3
-cnpm set registry http://localhost:7001
+/usr/bin/npm install -g --registry=http://registry.npm.taobao.org --disturl=http://npm.taobao.org/mirrors/node  cnpmjs.org cnpm sqlite3
+/usr/bin/cnpm set registry http://localhost:7001
